7205401dcfd9ad432944bac8edcb63833367a26a
Daniel Hormos <daniel_danne32@hotmail.com>
2024-10-15 21:41:38 +0200

    Created functions to check zombie count and also add zombie functions to make all test pass :tada:

--- src/zombies.test.ts
+++ src/zombies.test.ts
@@ -1,30 +1,76 @@
+import { deepEqual, strictEqual } from "node:assert";
 import { ok } from "node:assert/strict";
 import { test } from "node:test";
 
 const createRoom = (capacity: number) => {
   const _capacity = capacity;
+  let zombies = 0;
 
   return {
-    isFull: () => true,
+    CheckIfRoomFull: () => zombies >= capacity,
+
+    getZombieCount: () => zombies,
+
+    addZombie: () => {
+      if (zombies < _capacity) {
+        zombies++;
+      }
+    },
   };
 };
 
 test("room is full", () => {
   const room = createRoom(0);
 
-  const isRoomFull = room.isFull();
+  const isRoomFull = room.CheckIfRoomFull();
 
   ok(isRoomFull);
 });
 
-test.skip("empty room that fits one zombie is not full", () => {});
+test("room with no capacity cannot fit any zombies", () => {
+  const room = createRoom(0);
 
-test.skip("room with no capacity cannot fit any zombies", () => {});
+  room.addZombie();
 
-test.skip("one-roomer becomes full when a zombie is added", () => {});
+  deepEqual(room.getZombieCount(), 0);
+});
 
-test.skip("two-roomer is not full when a zombie is added", () => {});
+test("empty room that fits one zombie is not full", () => {
+  const room = createRoom(1);
 
-test.skip("second zombie consumes first zombie when added to a one-roomer", () => {});
+  const isRoomFull = room.CheckIfRoomFull();
+
+  ok(!isRoomFull);
+});
+
+test("one-roomer becomes full when a zombie is added", () => {
+  const room = createRoom(1);
+
+  room.addZombie();
+
+  const isRoomFull = room.CheckIfRoomFull();
+
+  ok(isRoomFull);
+});
+
+test("two-roomer is not full when a zombie is added", () => {
+  const room = createRoom(2);
+
+  room.addZombie();
+
+  const isRoomFull = room.CheckIfRoomFull();
+
+  ok(!isRoomFull);
+});
+
+test("second zombie consumes first zombie when added to a one-roomer", () => {
+  const room = createRoom(1);
+
+  room.addZombie();
+
+  room.addZombie();
+
+  deepEqual(room.getZombieCount(), 1);
+});
 
 // You are free to add more tests that you think are relevant!
